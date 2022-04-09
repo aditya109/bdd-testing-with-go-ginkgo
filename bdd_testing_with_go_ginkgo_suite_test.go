622e41c1c81ee3e938ac6995776f6b8d554e56fd
aditya109 <adikid1996@gmail.com>
2022-04-09 09:37:07 +0530

    completed the github actions integration for ci and reporting; solved #1; solved #2
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- bdd_testing_with_go_ginkgo_suite_test.go
+++ bdd_testing_with_go_ginkgo_suite_test.go
@@ -16,14 +16,11 @@ func TestBddTestingWithGoGinkgo(t *testing.T) {
 var agoutiDriver *agouti.WebDriver
 
 var _ = BeforeSuite(func() {
-	options := agouti.ChromeOptions("args", []string{
-		"--headless",
-	})
 	// Choose a WebDriver:
 
 	// agoutiDriver = agouti.PhantomJS()
 	// agoutiDriver = agouti.Selenium()
-	agoutiDriver = agouti.ChromeDriver(options)
+	agoutiDriver = agouti.ChromeDriver()
 
 	Expect(agoutiDriver.Start()).To(Succeed())
 })
